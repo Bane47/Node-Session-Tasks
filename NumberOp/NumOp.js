ab457d6887f1d4de0159a10744bcb636ea0668e2
Darshan <priyadarshanmanoharan@gmail.com>
2023-08-22 13:01:27 -0700

    Middleware logic updated

--- NumberOp/NumOp.js
+++ NumberOp/NumOp.js
@@ -1,34 +1,43 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-
 const app = express();
 app.use(bodyParser.json());
 
-//adding a middleware
-const middleware = function(req,res,next){
+// Adding a middleware
+const middleware = function (req, res, next) {
+  console.log("Welcome to the calculator--[This is a middleware]");
 
-console.log("Welcome to the calculator--[This is a middleware]");
-next();   //This will allow the data flow further
+  const { a, b } = req.query; // Extract a and b from the query parameters
 
+  if(!a||!b){
+    return res.status(500).json({
+      status:500,
+      error:"Please enter all fields"
+    })
+  }
+  if (isNaN(a) || isNaN(b)) {
+    return res.status(422).json({
+      status: 422,
+      error: "Wrong datatypes are passed in the query, Please"
+    });
+  }
+  next(); // This will allow the data flow further
 }
 
-app.use(middleware); //middlewares are used by the use(); and also this will work before the get() and post()
+app.use(middleware);
 
 app.get('/num', getMethod);
 app.post('/num', postMethod);
 
-
-
 function getMethod(req, res) {
   const { a, b, action } = req.query;
-
 
   let result;
   if (action === 'add') {
     result = parseInt(a) + parseInt(b);
-  } else if(action==='subtract') {
-    result = a - b;
+  } else if (action === 'subtract') {
+    result = parseInt(a) - parseInt(b);
   }
 
   res.json({ result });
@@ -40,7 +49,7 @@ function postMethod(req, res) {
   let result;
   if (action === 'add') {
     result = a + b;
-  } else if(action==='subtract') {
+  } else if (action === 'subtract') {
     result = a - b;
   }
 
@@ -48,5 +57,5 @@ function postMethod(req, res) {
 }
 
 app.listen(3000, () => {
-  console.log('runnin')
+  console.log('running');
 });
